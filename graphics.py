a43671abc4eb3aa2a24d37ef833917dbbb44d664
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2024-04-12 15:39:51 +0200

    update graphics.py
    3 classes created for graphs

--- graphics.py
+++ graphics.py
@@ -5,35 +5,43 @@ import numpy as np
 class Graphics:
     @staticmethod
     # create columns graph
-    def showBar(name_graph, x, y, x_name="", y_name=""):
+    def showBar(title: str, x: list, y: list, x_name = "", y_name = ""):
         x_np = np.array(x)
         y_np = np.array(y)
 
         plt.bar(x_np, y_np)
-        plt.title(name_graph)
+        plt.title(title)
         plt.xlabel(x_name)
         plt.ylabel(y_name)
         plt.show()
 
-Graphics.showBar("nom du graph", ["A", "B"], [1, 5], "x")
+    # create pie charts
+    def showPie(title: str, shares: list, mylabels = []):
+        y_np = np.array(shares)
 
-# create pie charts
-y = np.array([35, 25, 25, 15])
-mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+        plt.pie(y_np, labels = shares)
+        plt.legend(mylabels)
+        plt.title(title)
+        plt.show()
 
-plt.pie(y, labels = mylabels)
-plt.legend(title = "Four Fruits:")
-plt.show()
+    #create 2 sets of scatter plots for comparison
+    def show2setsPlots(title: str, values: list):
+        #instance one
+        for value in values:
+            x = []
+            y = []
+            for point in value:
+                x.append(point[0])
+                y.append(point[1])
+            x_np = np.array(x)
+            y_np = np.array(y)
+            plt.scatter(x_np, y_np)
+        plt.show()
 
-#create 2 sets of scatter plots for comparison
-#day one, the age and speed of 13 cars:
-x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-plt.scatter(x, y)
-
-#day two, the age and speed of 15 cars:
-x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
-y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
-plt.scatter(x, y)
-
-plt.show()
+if __name__ == "__main__":
+    Graphics.showBar("nom du graph", ["A", "B"], [1, 5], "x")
+    Graphics.showPie("nom graph", [35, 15, 23, 27], ["chiens", "chats", "poissons", "chevaux"])
+    Graphics.show2setsPlots("nom graphe", [
+        [(1,5),(2,7)],
+        [(5,6),(9,5),(6,1)]
+        ])
