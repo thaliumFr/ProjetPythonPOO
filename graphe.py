f2e3b1cd4a21110371dbc0e809d9ea5c1bf97874
N4suk4 <sebastien.benoit9@gmail.com>
2024-04-15 18:01:25 +0200

    graphe.py a encore des prooblème...j'ai fait ce que j'ai pu désolé

--- graphe.py
+++ graphe.py
@@ -1,26 +1,77 @@
-from matplotlib import axes, projections
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import csv
+from SaveSystem import Csv
+from dataImporter import DataImporter
 
-class Gaphics3D:
+
+class Graphics3D:
+    
     @staticmethod
-    def showgraph(name_graph, x, y, z, x_name= "", y_name="", z_name=""):
+    def historigram3D():
+        data = Csv.Load("OpenCritics")
         fig = plt.figure()
-        ax = fig.add_subplot(111, projections="3d") 
-        # la ligne du dessus ne fonctionne pas
+        ax = fig.add_subplot(projection='3d')
+        dataImportOC = Csv.Load("OpenCritics")
 
-        x_np = np.array(x)
-        y_np = np.array(y)
-        z_np = np.array(z)
+        devs = dataImportOC.GetColumn(2)
+        release = dataImportOC.GetColumn(3)
+       
+        
 
-        ax.bar3d(x_np, y_np, np.zeros_like(x_np), 0.5, 0.5, z_np, zsort='average')
+        hist, xedges, yedges = np.histogram2d(devs, release, bins=10, range=[[min(devs), max(devs)], [min(release), max(release)]])
 
-        ax.set_title(name_graph)
-        ax.set_xlabel(x_name)
-        ax.set_ylabel(y_name)
-        ax.set_zlabel(z_name)
+            # Construct arrays for the anchor positions of the 16 bars.
+        xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25, indexing="ij")
+        xpos = xpos.ravel()
+        ypos = ypos.ravel()
+        zpos = 0
 
-        plt.show()
+            # Construct arrays with the dimensions for the 16 bars.
+        dx = dy = 0.5 * np.ones_like(zpos)
+        dz = hist.ravel()
 
-    showgraph("3D", 1,2,3,"a", "b", "c")
+        ax.bar3d(xpos, ypos, zpos, dx, dy, dz, zsort='average')
+
+        ax.set_xlabel('Studio')
+        ax.set_ylabel('Année')
+        ax.set_zlabel('Prix')
+
+        
+        ax.set_xticks(devs)
+        ax.set_xticklabels([data.content[i][2] for i in range(2)], rotation=75)
+
+
+    @staticmethod
+    def scatterplot():
+        np.random.seed(19680801)
+
+
+        def randrange(n, vmin, vmax):
+            return (vmax - vmin)*np.random.rand(n) + vmin
+
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+
+        n = 100
+
+        # For each set of style and range settings, plot n random points in the box
+        # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+        for m, zlow, zhigh in [('o', -50, -25), ('^', -30, -5)]:
+            xs = randrange(n, 1, 12)
+            ys = randrange(n, 2014, 2024)
+            zs = randrange(n, zlow, zhigh)
+            ax.scatter(xs, ys, zs, marker=m)
+
+        ax.set_xlabel('Mois')
+        ax.set_ylabel('Année')
+        ax.set_zlabel('Quantité')
+ 
+
+# Appel de la méthode historigram3D de manière statique
+Graphics3D.historigram3D()
+
+# Appel de la méthode scatterplot de manière statique
+Graphics3D.scatterplot()
+
+plt.show()
